249873b476c34ae6f07b4b1acaff7adb483f2e79
Nono1050 <abdelkadernordine@hotmail.com>
2025-02-19 11:21:38 +0100

    chemin entre fichiers .php et la page web du wamp

---
+++ templates/accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>accueil</title>
+</head>
+<body>
+    <h1>accueil</h1>
+    <ul>
+        <li><a href="./">accueil</a></li>
+        <li><a href="./?p=form">contact</a></li>
+        <li><a href="./?p=cult">culture</a></li>
+        <li><a href="./?p=gal">galerie</a></li>
+        <li><a href="./?p=geo">geographie</a></li>
+        <li><a href="./?p=hist">histoire</a></li>
+        <li><a href="./?p=link">liens</a></li>
+    </ul>
+    
+</body>
+</html>
---
+++ templates/page-404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page-404-error</title>
+</head>
+<body>
+    <h1>page-404-error</h1>
+    
+</body>
+</html>
